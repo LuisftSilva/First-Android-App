70d0375345a9f1b00c5dd6f2fb730fb52e8e4cfd
Luís Silva <ftsluis@gmail.com>
2018-01-05 15:47:57 +0000

    Added return of the sent message in a second activity

--- app/src/main/java/com/example/lfts/myfirstandroidapp/MainActivity.java
+++ app/src/main/java/com/example/lfts/myfirstandroidapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.lfts.myfirstandroidapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public static final String EXTRA_MESSAGE = "com.example.lfts.myfirstandroidapp.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("fodasse");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        System.out.println("ola ola");
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
